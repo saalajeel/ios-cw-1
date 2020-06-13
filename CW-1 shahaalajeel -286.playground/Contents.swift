a54d7844da0bbb9d6ad6d9550c2b2c1d7f4c48c3
Shaha Alajeel <saalajeel@gmail.com>
2020-06-13 19:22:04 +0300

    cw1completed!

---
+++ CW-1 shahaalajeel -286.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let name = "Shaha"
+var hobby1 = "playing video games"
+var hobby2 = "reading"
+var age = "15"
+var height = "158"
+print ("Hi, my name is \(name). I like \(hobby1) and \(hobby2). I am \(age)-years-old and I am \(height) cm tall.")
